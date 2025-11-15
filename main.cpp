b273f6fbf82288bc0db7d8ebfd969ebca9d9c587
Soman Abbasi <mssabbasi306@gmail.com>
2025-11-15 10:00:43 +0500

    Instruction Prototype for team members

--- main.cpp
+++ main.cpp
@@ -16,5 +16,38 @@ HostelExpenseTracker/
       ├── storage.cpp
       └── menu.cpp
 
+*/
+
+/*
+How to push on main brach
+git add .
+git commit -m "Somes message you did changes"
+git push
+
+*/
+
+/*
+=> Branch Workflow for Team Members
+
+git checkout -b feature/branch-name 
+
+
+
+=> How to push on branch
+git add .
+git commit -m "Some messages You did changes"
+git push origin feature/branch-name
+
+
+*/
+
+
+/*
+=> Update Local Repository
+
+Before creating a new branch or working again, make sure your local main is up to date:
+
+git checkout main
+git pull origin main
 
 */
