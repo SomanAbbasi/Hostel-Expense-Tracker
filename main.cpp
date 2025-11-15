def7fbf11b7e385bcd1367a3f64b0d908759bc4d
Soman Abbasi <mssabbasi306@gmail.com>
2025-11-15 09:47:43 +0500

    OOP Project - Hostellite Expense Tracker

---
+++ main.cpp
@@ -0,0 +1,20 @@
+/*
+HostelExpenseTracker/
+│
+├── include/
+│     ├── students.h
+│     ├── expenses.h
+│     ├── reports.h
+│     ├── storage.h
+│     └── menu.h
+│
+└── src/
+      ├── main.cpp
+      ├── students.cpp
+      ├── expenses.cpp
+      ├── reports.cpp
+      ├── storage.cpp
+      └── menu.cpp
+
+
+*/
